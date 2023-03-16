278496a299db2f1be05f5532efc92ba9ed97aab1
Trevor <trevor.chong@xilnex.com>
2023-03-16 17:32:16 +0800

    half way uninterested

--- main.go
+++ main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, "Go!")
+}
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
