01b41f270123d2a636215bb404beba5d99e6872f
Trevor <trevor.chong@xilnex.com>
2023-03-16 11:29:03 +0800

    init practice 6 pointers and errors

--- main.go
+++ main.go
@@ -1,40 +1,4 @@
 package main
 
-import "math"
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-//func Area(rectangle Rectangle) float64 {
-//	return rectangle.Width * rectangle.Height
-//}
-
-type Shape interface {
-	Area() float64
-}
-
-type Rectangle struct {
-	Width  float64
-	Height float64
-}
-
-type Circle struct {
-	Radius float64
-}
-
-type Triangle struct {
-	Base   float64
-	Height float64
-}
-
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
-}
-func (c Circle) Area() float64 {
-	return math.Pi * math.Pow(c.Radius, 2)
-}
-
-func (t Triangle) Area() float64 {
-	return (t.Base * t.Height) * 0.5
+type Wallet struct {
 }
